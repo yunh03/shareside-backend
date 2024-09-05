53f888933a61ca6c2f4877b3dbe4d50f145c8808
Yun-Hwan Jeon <yunsol267@gmail.com>
2024-09-05 14:24:30 +0000

    ✨Add Tests

---
+++ src/main/java/kr/yuns/spring_initialize/domain/test/controller/TestController.java
@@ -0,0 +1,34 @@
+package kr.yuns.spring_initialize.domain.test.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.yuns.spring_initialize.domain.test.data.dto.TestRequestDto;
+import kr.yuns.spring_initialize.domain.test.service.TestService;
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/test")
+@AllArgsConstructor
+public class TestController {
+    private TestService testService;
+
+    @GetMapping("/")
+    public ResponseEntity<?> test() {
+        return ResponseEntity.status(HttpStatus.OK).body("test");
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<?> saveTestEntity(@RequestParam String title, String content) {
+        return testService.saveTestEntity(
+            TestRequestDto.builder()
+                .title(title)
+                .content(content)
+        .build());
+    }
+}
---
+++ src/main/java/kr/yuns/spring_initialize/domain/test/data/entity/Test.java
@@ -0,0 +1,32 @@
+package kr.yuns.spring_initialize.domain.test.data.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(callSuper = true)
+@Table(name = "test")
+@Builder
+public class Test {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String contents;
+}
