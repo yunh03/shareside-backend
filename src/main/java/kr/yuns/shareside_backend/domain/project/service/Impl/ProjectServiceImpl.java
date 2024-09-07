4ca8cda40c2a090511b189e0602dca064888c121
Yun-Hwan Jeon <yunsol267@gmail.com>
2024-09-07 07:51:25 +0000

    ✨Project service basic design

---
+++ src/main/java/kr/yuns/shareside_backend/domain/project/service/Impl/ProjectServiceImpl.java
@@ -0,0 +1,8 @@
+import org.springframework.stereotype.Service;
+
+import kr.yuns.shareside_backend.domain.project.service.ProjectService;
+
+@Service
+public class ProjectServiceImpl implements ProjectService {
+    
+}
